63955f73a188d99ded231aa85b1ac43b751c98ce
Yunkai Wang <yunkaiwang1996@gmail.com>
2018-01-25 23:06:20 -0500

    add menu print and scanner to client

--- src/project/TFTPClient.java
+++ src/project/TFTPClient.java
@@ -1,5 +1,36 @@
 package project;
 
-public class TFTPClient {
+import java.util.Scanner;
 
+public class TFTPClient {
+	TFTPClient() {
+	}
+	
+	private static void printMenu() {
+		System.out.println("Available commands:");
+		System.out.println("1. stop - stop the client");
+		System.out.println();
+	}
+	
+	private void waitForCommand() {
+		Scanner s = new Scanner(System.in);
+		while (true) {
+			printMenu();
+			System.out.print("Command: ");
+			String cmdLine = s.nextLine().toLowerCase();
+			switch(cmdLine) {
+				case "stop":
+					System.out.println("Stopping client...Good bye!");
+					s.close();
+					return;
+				default:
+					System.out.println("Invalid command, please try again!\n");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		TFTPClient client = new TFTPClient();
+		client.waitForCommand();
+	}
 }
